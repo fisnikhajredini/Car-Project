326c1a8a09008795f6fe780a2db9129e47843c1c
Aksel Strömberg <aksel.stromberg@gmail.com>
2016-04-08 13:02:19 +0200

    Update Address_changer_for_pins.ino

--- Address_changer_for_pins.ino
+++ Address_changer_for_pins.ino
@@ -1,3 +1,9 @@
+/*
+This code was found online and was used to change the address of a pin since 2 different SCL,SDA pins had the same address.
+Credit iToby
+http://forum.arduino.cc/index.php?topic=124333.0
+*/
+
 #include <Wire.h>
 
 #define oldad (0xE6 >>1)
