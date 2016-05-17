bef033ce42f55de9f9fe562c4928f4f6301982b4
aksel <Aksel.stromberg@gmail.com>
2016-05-17 13:44:59 +0200

    adding shit

--- parking code/simulation code /driver/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/steve/OpenDaVINCI/automotive/miniature/driver
-# Build directory: /home/steve/OpenDaVINCI/automotive/miniature/driver/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(DriverTestSuite.h-TestSuite "/home/steve/OpenDaVINCI/automotive/miniature/driver/build/DriverTestSuite.h-TestSuite")
-SET_TESTS_PROPERTIES(DriverTestSuite.h-TestSuite PROPERTIES  TIMEOUT "3000")
